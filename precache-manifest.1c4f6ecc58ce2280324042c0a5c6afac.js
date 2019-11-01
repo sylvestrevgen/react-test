0e87001a2e0842bbf3180adeb48b1ecf985e8037
sylvestrevgen <sylvestrevgen@gmail.com>
2019-11-01 23:09:28 +0200

    Updates

---
+++ precache-manifest.1c4f6ecc58ce2280324042c0a5c6afac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91bcdb0ef229f31271f53f4a7fec2c3",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "560752b883329f252533",
+    "url": "/react-test/static/css/0.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "26dc4552104b913bf79b",
+    "url": "/react-test/static/css/main.164e7c83.chunk.css"
+  },
+  {
+    "revision": "276a0a656aa42eb7cd21",
+    "url": "/react-test/static/css/posts-page.b178b624.chunk.css"
+  },
+  {
+    "revision": "4d8a7984e5f801f2fa27",
+    "url": "/react-test/static/css/single-post-page.d81a4292.chunk.css"
+  },
+  {
+    "revision": "560752b883329f252533",
+    "url": "/react-test/static/js/0.5a63ed9f.chunk.js"
+  },
+  {
+    "revision": "0277b2ee30bd22af823f",
+    "url": "/react-test/static/js/5.c1d4be73.chunk.js"
+  },
+  {
+    "revision": "26dc4552104b913bf79b",
+    "url": "/react-test/static/js/main.eb021eca.chunk.js"
+  },
+  {
+    "revision": "276a0a656aa42eb7cd21",
+    "url": "/react-test/static/js/posts-page.64693166.chunk.js"
+  },
+  {
+    "revision": "5f1122c465b3b26ddfe8",
+    "url": "/react-test/static/js/runtime-main.32f6b182.js"
+  },
+  {
+    "revision": "4d8a7984e5f801f2fa27",
+    "url": "/react-test/static/js/single-post-page.a9b4a010.chunk.js"
+  }
+]);
